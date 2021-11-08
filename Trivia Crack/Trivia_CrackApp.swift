557209de05f3d8ed2429dbe0ef213550c5bc62e7
MAS00N <66094125+MAS00N@users.noreply.github.com>
2021-11-08 13:24:39 -0600

    Initial Commit

---
+++ Trivia Crack/Trivia_CrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_CrackApp.swift
+//  Trivia Crack
+//
+//  Created by Mason Dierkes on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_CrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
